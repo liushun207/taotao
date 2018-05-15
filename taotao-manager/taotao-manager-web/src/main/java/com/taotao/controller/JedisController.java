589b9a67c5c979deae438f1d79faeeae78941f81
刘顺 <642517492@qq.com>
2018-05-15 15:54:14 +0800

    新增 redis 连接及测试

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/JedisController.java
@@ -0,0 +1,44 @@
+package com.taotao.controller;
+
+import com.taotao.common.jedis.JedisClient;
+import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.pojo.TbItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * Jedis 缓存测试
+ */
+@Controller
+public class JedisController
+{
+    /**
+     * 注入jedis 客户端
+     */
+    @Autowired
+    private JedisClient jedisClient;
+
+    private String key = "test";
+
+    @RequestMapping("/jedis/set")
+    @ResponseBody
+    public Boolean setJedis()
+    {
+        String str = "测试";
+        jedisClient.set(key, str);
+        jedisClient.hset(key + "1", 1 + "", str);
+        return true;
+    }
+
+    @RequestMapping("/jedis/get")
+    @ResponseBody
+    public String getJedis()
+    {
+        String result = jedisClient.hget(key + "1", 1 + "");
+        return result;
+    }
+}
