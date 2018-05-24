18a83d077d2b85c38d9423b0eac35da08c7b8a15
曾智 <service@unnnn.cn>
2018-05-24 15:03:34 +0800

    增加控制器注释。

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/AbstractApiController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/AbstractApiController.java
@@ -1,9 +1,13 @@
 package com.taotao.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- * @author:
- * @Description:
+ * 控制器基类。
+ * @author:曾智
+ * @Description:控制器基类，提供公共的操作方法。
  * @Data: 2018/5/24 14:45
  **/
-public class AbstractApiController {
+@RestController
+public abstract class AbstractApiController {
 }
--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/OrderController.java
+++
@@ -1,9 +0,0 @@
-package com.taotao.controller;
-
-/**
- * @author:
- * @Description:
- * @Data: 2018/5/24 14:48
- **/
-public class OrderController {
-}
