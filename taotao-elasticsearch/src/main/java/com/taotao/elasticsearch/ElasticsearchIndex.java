224fc424517d4833cc52e10378e8675cf5e6e917
刘顺 <642517492@qq.com>
2018-06-06 17:54:03 +0800

    修改配置

---
+++ taotao-elasticsearch/src/main/java/com/taotao/elasticsearch/ElasticsearchIndex.java
@@ -0,0 +1,13 @@
+package com.taotao.elasticsearch;
+
+import org.elasticsearch.client.transport.TransportClient;
+
+/**
+ * @author:
+ * @Description:
+ * @Data: 2018/6/6 17:42
+ **/
+public class ElasticsearchIndex
+{
+    private TransportClient client;
+}
